c114ebd42c1421d3387b26a6397dd979baef6954
塗 慶誉 <123852757+Tokeiyo929@users.noreply.github.com>
2025-11-19 14:26:25 +0800

    Add game companies data
    This file contains data about various game companies, including their names, countries, cities, number of employees, and geographical coordinates.

---
+++ gameCompaniesData.js
@@ -0,0 +1,195 @@
+// 游戏公司数据
+const gameCompaniesData = [
+    {
+        name: "腾讯游戏",
+        country: "中国",
+        city: "深圳",
+        employees: 50000,
+        lat: 22.5431,
+        lng: 114.0579
+    },
+    {
+        name: "网易游戏",
+        country: "中国",
+        city: "广州",
+        employees: 20000,
+        lat: 23.1291,
+        lng: 113.2644
+    },
+    {
+        name: "米哈游",
+        country: "中国",
+        city: "上海",
+        employees: 4000,
+        lat: 31.2304,
+        lng: 121.4737
+    },
+    {
+        name: "任天堂",
+        country: "日本",
+        city: "京都",
+        employees: 6500,
+        lat: 34.9858,
+        lng: 135.7586
+    },
+    {
+        name: "索尼互动娱乐",
+        country: "日本",
+        city: "东京",
+        employees: 9000,
+        lat: 35.6762,
+        lng: 139.6503
+    },
+    {
+        name: "动视暴雪",
+        country: "美国",
+        city: "尔湾",
+        employees: 9500,
+        lat: 33.6846,
+        lng: -117.8265
+    },
+    {
+        name: "艺电",
+        country: "美国",
+        city: "红木城",
+        employees: 12900,
+        lat: 37.4852,
+        lng: -122.2364
+    },
+    {
+        name: "育碧",
+        country: "法国",
+        city: "巴黎",
+        employees: 20300,
+        lat: 48.8566,
+        lng: 2.3522
+    },
+    {
+        name: "CD Projekt",
+        country: "波兰",
+        city: "华沙",
+        employees: 1100,
+        lat: 52.2297,
+        lng: 21.0122
+    },
+    {
+        name: "Supercell",
+        country: "芬兰",
+        city: "赫尔辛基",
+        employees: 350,
+        lat: 60.1699,
+        lng: 24.9384
+    },
+    {
+        name: "拳头游戏",
+        country: "美国",
+        city: "洛杉矶",
+        employees: 2500,
+        lat: 34.0522,
+        lng: -118.2437
+    },
+    {
+        name: "Epic Games",
+        country: "美国",
+        city: "卡里",
+        employees: 3200,
+        lat: 35.7915,
+        lng: -78.7811
+    },
+    {
+        name: "莉莉丝游戏",
+        country: "中国",
+        city: "上海",
+        employees: 1500,
+        lat: 31.2304,
+        lng: 121.4737
+    },
+    {
+        name: "完美世界",
+        country: "中国",
+        city: "北京",
+        employees: 5000,
+        lat: 39.9042,
+        lng: 116.4074
+    },
+    {
+        name: "Capcom",
+        country: "日本",
+        city: "大阪",
+        employees: 2900,
+        lat: 34.6937,
+        lng: 135.5023
+    },
+    {
+        name: "Square Enix",
+        country: "日本",
+        city: "东京",
+        employees: 4500,
+        lat: 35.6762,
+        lng: 139.6503
+    },
+    {
+        name: "万代南梦宫",
+        country: "日本",
+        city: "东京",
+        employees: 7500,
+        lat: 35.6762,
+        lng: 139.6503
+    },
+    {
+        name: "微软游戏",
+        country: "美国",
+        city: "雷德蒙德",
+        employees: 7000,
+        lat: 47.6740,
+        lng: -122.1215
+    },
+    {
+        name: "Valve",
+        country: "美国",
+        city: "贝尔维尤",
+        employees: 360,
+        lat: 47.6101,
+        lng: -122.2015
+    },
+    {
+        name: "网易日本",
+        country: "中国",
+        city: "东京",
+        employees: 200,
+        lat: 35.6762,
+        lng: 139.6503
+    },
+    {
+        name: "腾讯光子工作室",
+        country: "中国",
+        city: "深圳",
+        employees: 2000,
+        lat: 22.5431,
+        lng: 114.0579
+    },
+    {
+        name: "腾讯天美工作室",
+        country: "中国",
+        city: "成都",
+        employees: 3000,
+        lat: 30.5728,
+        lng: 104.0668
+    },
+    {
+        name: "游戏科学",
+        country: "中国",
+        city: "深圳",
+        employees: 400,
+        lat: 22.5431,
+        lng: 114.0579
+    },
+    {
+        name: "帕斯亚科技",
+        country: "中国",
+        city: "重庆",
+        employees: 150,
+        lat: 29.5630,
+        lng: 106.5516
+    }
+];
